404509da386fe5d3d45a4986ecd11c487fc74453
Haoyu Liu <liuhaoyuca@gmail.com>
2017-06-24 20:27:51 +0000

    basic register

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Travel!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :last_name, :email, :password,
+                                   :password_confirmation, :title, :department, :image, :tel, :bio)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
-    get 'static_pages/home'
+    
+    get 'users/index'
+    
+    get 'users/show'
 
-    get 'static_pages/about'
+    get 'users/new'
+
+    get 'home' ,to: 'static_pages#home'
+    
+    get 'about' ,to: 'static_pages#about'
+    
+    get 'signup' ,to: 'users#new'
 
     root 'static_pages#home'
+
+
+    resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
