bfec4aeafe8657f623db2c66fd3031bf2b726c57
Andy Tang <andeetang@gmail.com>
2017-06-24 17:20:36 -0700

    Revert user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
-#    has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
-#  validates_attachment :image, :presence => true,
-#  :content_type => { content_type: ["image/jpg","image/jpeg","image/png","image/gif"] },
-# =>   :size => { :in => 0..100.megabytes }
+    has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
+  validates_attachment :image, :presence => true,
+  :content_type => { content_type: ["image/jpg","image/jpeg","image/png","image/gif"] },
+ =>   :size => { :in => 0..100.megabytes }
 end
